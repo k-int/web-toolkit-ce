cea0ad5c94ae7262e0095420194fdbeb1ef088ec
Steve Osguthorpe <steve.osguthorpe@k-int.com>
2018-04-11 13:00:03 +0100

    Namespace the config URL mappings to avoid conflicts.

--- grails-app/controllers/com/k_int/web/toolkit/UrlMappings.groovy
+++ grails-app/controllers/com/k_int/web/toolkit/UrlMappings.groovy
@@ -3,9 +3,9 @@ package com.k_int.web.toolkit
 class UrlMappings {
 
   static mappings = {
-    "/config/$extended?" (controller: 'config' , action: "resources")
-    "/config/schema/$type" (controller: 'config' , action: "schema")
-    "/config/schema/embedded/$type" (controller: 'config' , action: "schemaEmbedded")
+    "/kiwt/config/$extended?" (controller: 'config' , action: "resources")
+    "/kiwt/config/schema/$type" (controller: 'config' , action: "schema")
+    "/kiwt/config/schema/embedded/$type" (controller: 'config' , action: "schemaEmbedded")
     
     "/ref/blank/$domain/$prop" (controller: 'ref', action: 'blank')
   }
