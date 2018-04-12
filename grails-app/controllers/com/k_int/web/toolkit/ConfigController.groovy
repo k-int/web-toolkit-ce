00515ebdbb16516b29c966f42e2e7f7cf7b89a81
Steve Osguthorpe <steve.osguthorpe@k-int.com>
2018-04-12 14:58:32 +0100

    Tweaks to controller to work with host used in the request.

--- grails-app/controllers/com/k_int/web/toolkit/ConfigController.groovy
+++ grails-app/controllers/com/k_int/web/toolkit/ConfigController.groovy
@@ -1,7 +1,7 @@
 package com.k_int.web.toolkit
 
 import java.util.Map
-
+import javax.servlet.http.HttpServletRequest
 import com.k_int.web.toolkit.json.JsonSchemaUtils
 import com.k_int.web.tools.RestfulResourceService
 
@@ -12,28 +12,36 @@ class ConfigController {
   static responseFormats = ['json']
   RestfulResourceService restfulResourceService
   
-  def resources () {
+  private String getRootLink(String type) {
+    String theUri = request.forwardURI.toLowerCase().replaceAll("${type.toLowerCase()}(\\/?)\$", "").replaceAll(/\/\//, '/')
     
+    String link = request.getHeader('host')
+    
+    if (!link) {
+      link = grailsLinkGenerator.link(absolute: true, uri: theUri)
+    } else {
+      String proto = request.getHeader("X-Forwarded-Proto") ?: request.scheme
+      link = "${proto}://${link}${theUri}"
+    }
+    
+    link
+  }
+  
+  def resources () {
     boolean extended = params.get('extended') == 'extended'
-        
     render ([
       resources : restfulResourceService.getResourceInfo (extended)
     ] as JSON )
   }
   
   def schemaEmbedded (String type) {
-
-    String theUri = request.forwardURI.toLowerCase().replaceAll("${type.toLowerCase()}(\\/?)\$", "")
-    
-    def schema = JsonSchemaUtils.jsonSchema(type, grailsLinkGenerator.link(absolute: true, uri: theUri), true)
+    def schema = JsonSchemaUtils.jsonSchema(type, getRootLink(type), true)
     render schema as JSON
   }
   
   
   def schema (String type) {
-
-    String theUri = request.forwardURI.toLowerCase().replaceAll("${type.toLowerCase()}(\\/?)\$", "")
-    def schema = JsonSchemaUtils.jsonSchema(type, grailsLinkGenerator.link(absolute: true, uri: theUri) ,false)
+    def schema = JsonSchemaUtils.jsonSchema(type, getRootLink(type) ,false)
     render schema as JSON
   }
 }
