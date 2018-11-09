9f7dd7ca7dd379c39c09b84521a3c830d131fbc6
Steve Osguthorpe <steve.osguthorpe@k-int.com>
2018-11-09 19:07:37 +0000

    Name now on def.

--- src/main/groovy/com/k_int/web/toolkit/custprops/CustomProperty.groovy
+++ src/main/groovy/com/k_int/web/toolkit/custprops/CustomProperty.groovy
@@ -9,11 +9,11 @@ import grails.gorm.transactions.Rollback
 @Entity
 class CustomProperty<T> implements MultiTenant<CustomProperty> {
   
-  String name
+//  String name
   CustomPropertyDefinition definition
   T value
   
   static mapping = {
-    tablePerHierarchy true
+    tablePerHierarchy false
   }
 }
