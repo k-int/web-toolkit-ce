7dad601a978fdb2df2cee8e458e18ee61837845d
Steve Osguthorpe <steve.osguthorpe@k-int.com>
2018-11-15 14:43:21 +0000

    Custom property definition controller.

--- grails-app/controllers/com/k_int/web/toolkit/CustomPropertyDefinitionController.groovy
+++ grails-app/controllers/com/k_int/web/toolkit/CustomPropertyDefinitionController.groovy
@@ -3,6 +3,11 @@ package com.k_int.web.toolkit
 import com.k_int.web.toolkit.custprops.CustomPropertyDefinition
 import com.k_int.web.toolkit.rest.RestfulController
 
+import grails.web.Controller
+import groovy.util.logging.Slf4j
+
+@Slf4j
+@Controller
 class CustomPropertyDefinitionController extends RestfulController<CustomPropertyDefinition> {
   CustomPropertyDefinitionController() {
     super(CustomPropertyDefinition)
