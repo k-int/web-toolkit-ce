f0b055506b054c7be0f414c41aa6ac7d345621d7
Ian Ibbotson <ianibbo@googlemail.com>
2019-09-27 14:47:22 +0100

    Added AppSetting

---
+++ src/main/groovy/com/k_int/web/toolkit/settings/AppSetting.groovy
@@ -0,0 +1,38 @@
+package com.k_int.web.toolkit.settings
+
+
+import grails.gorm.MultiTenant
+import grails.gorm.annotation.Entity
+
+@Entity
+class AppSetting implements MultiTenant<AppSetting> {
+
+  String id
+  String section
+  String key
+  String settingType
+  String vocab
+  String defValue
+  String value
+
+  static mapping = {
+             id column: 'st_id', generator: 'uuid', length:36
+        version column: 'st_version'
+        section column: 'st_section'
+            key column: 'st_key'
+    settingType column: 'st_setting_type'
+          vocab column: 'st_vocab'
+       defValue column: 'st_default_value'
+          value column: 'st_value'
+  }
+
+  static constraints = {
+        section (nullable: true,  blank: false)
+            key (nullable: false, blank: false)
+    settingType (nullable: true,  blank: false)
+          vocab (nullable: true,  blank: false)
+       defValue (nullable: true,  blank: false)
+          value (nullable: true,  blank: true)
+  }
+  
+}
