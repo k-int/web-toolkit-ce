06a6a91b54f5ccd7710b5837cce86dc31d96f43f
EthanFreestone <54310740+EthanFreestone@users.noreply.github.com>
2025-08-04 09:57:12 +0100

    fix: Wrap error controller logic in try/catch to prevent infinite looping (#22)

--- grails-app/controllers/com/k_int/web/toolkit/ErrorController.groovy
+++ grails-app/controllers/com/k_int/web/toolkit/ErrorController.groovy
@@ -12,37 +12,43 @@ class ErrorController {
   GrailsApplication grailsApplication // Inject GrailsApplication
 
   def handle500() {
-    Throwable ex = request.getAttribute('javax.servlet.error.exception') ?: request.getAttribute('exception')
-    if (ex?.cause) {
-      ex = ex.cause
+    try {
+      Throwable ex = request.getAttribute('javax.servlet.error.exception') ?: request.getAttribute('exception')
+      if (ex?.cause) {
+        ex = ex.cause
+      }
+
+      String message = "Uncaught Internal server error";
+      int code = 500;
+
+      // Individual error handling. If exception implements errorHandleable500 then we can get a String message from it
+      if (ex instanceof handledException) {
+        ErrorHandle err = ex.handleException()
+        message = err.message;
+        code = err.code;
+      }
+
+
+      // Fetch config property from the application using the plugin
+      Boolean includeStack = Boolean.valueOf(grailsApplication.config.getProperty('endpoints.include-stack-trace', String, 'false'))
+
+      def model = [
+        timestamp    : Instant.now().toString(),
+        exception    : ex,
+        includeStack : includeStack,
+        message      : message,
+        code         : code
+      ]
+
+      response.status = code;
+      // Explicitly render the standard '/error' view name.
+      // Grails will look for /grails-app/views/error.gson first in the app,
+      // then fall back to the one in the plugin.
+      render(view: '/error', model: model)
+    } catch (Exception e) {
+      // If we fail to handle the error, we log it and return a generic error message
+      log.error("Error handling 500 error: ${e.message}", e)
+      render(status: 500, text: "A fatal error occurred handling 500 error.")
     }
-
-    String message = "Uncaught Internal server error";
-    int code = 500;
-
-    // Individual error handling. If exception implements errorHandleable500 then we can get a String message from it
-    if (ex instanceof handledException) {
-      ErrorHandle err = ex.handleException()
-      message = err.message;
-      code = err.code;
-    }
-
-
-    // Fetch config property from the application using the plugin
-    Boolean includeStack = Boolean.valueOf(grailsApplication.config.getProperty('endpoints.include-stack-trace', String, 'false'))
-
-    def model = [
-      timestamp    : Instant.now().toString(),
-      exception    : ex,
-      includeStack : includeStack,
-      message      : message,
-      code         : code
-    ]
-
-    response.status = code;
-    // Explicitly render the standard '/error' view name.
-    // Grails will look for /grails-app/views/error.gson first in the app,
-    // then fall back to the one in the plugin.
-    render(view: '/error', model: model)
   }
 }
